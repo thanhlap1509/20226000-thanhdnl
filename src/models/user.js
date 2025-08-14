f515873c96960c31ac220a678a23ce605a58c761
Dinh Ngoc Lap Thanh <thanhdinhngoclap123@gmail.com>
2025-08-14 09:14:44 +0700

    add timestamp to document

--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    validate: (value) => {
-      if (!validator.isEmail(value)) {
-        throw new Error("Invalid email");
-      }
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate: (value) => {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
     },
   },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-});
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("User", userSchema);
