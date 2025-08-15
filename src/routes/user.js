d98500ae9061ebc7bff5ffd249b2b3247125a039
Dinh Ngoc Lap Thanh <thanhdinhngoclap123@gmail.com>
2025-08-15 19:04:47 +0700

    standardize validation schema name

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { validateUserData, validateUserId, validateUserDocument } = require("../validation");
+const { createUsers, queryUserId, updateUser } = require("../validation");
 const userController = require("../controllers");
 
-router.route("/").get(userController.getAllUsers).post(validateUserData, userController.createUser);
+router.route("/").get(userController.getAllUsers).post(createUsers, userController.createUser);
 
 router.route("/count").get(userController.getUserCount);
 
 router
   .route("/:userId")
-  .get(validateUserId, userController.getUser)
-  .put(validateUserDocument, userController.updateUser)
-  .delete(validateUserId, userController.deleteUser);
+  .get(queryUserId, userController.getUser)
+  .put(updateUser, userController.updateUser)
+  .delete(queryUserId, userController.deleteUser);
 
 module.exports = router;
--- src/validation/user.js
+++ src/validation/user.js
@@ -1,37 +1,37 @@
 // check đầu vào, khi đi qua validation tức đầu vào là đúng và có thể xử lý được
 const { Joi, validate } = require("express-validation");
 
-const singleUserSchema = Joi.object({
+const createUser = Joi.object({
   email: Joi.string().email().trim().lowercase().required(),
   password: Joi.string().trim().required(),
   role: Joi.string().trim().required().valid("admin", "user"),
 });
 
-const userData = {
+const createUsers = {
   body: Joi.alternatives()
     .try(
-      singleUserSchema, // single object
-      Joi.array().items(singleUserSchema), // array of objects
+      createUser, // single object
+      Joi.array().items(createUser), // array of objects
     )
     .required(),
 };
 
-const userId = {
+const queryUserId = {
   params: Joi.object({
     userId: Joi.string().trim().required(),
   }).required(),
 };
 
-const userDocument = {
-  params: userId.params,
-  body: singleUserSchema
+const updateUser = {
+  params: queryUserId.params,
+  body: createUser
     .fork(["email", "password", "role"], (schema) => schema.required())
     .or("email", "password", "role")
     .required(),
 };
 
 module.exports = {
-  validateUserId: validate(userId, { keyByField: true }),
-  validateUserData: validate(userData, { keyByField: true }),
-  validateUserDocument: validate(userDocument, { keyByField: true }),
+  queryUserId: validate(queryUserId, { keyByField: true }),
+  createUsers: validate(createUsers, { keyByField: true }),
+  updateUser: validate(updateUser, { keyByField: true }),
 };
