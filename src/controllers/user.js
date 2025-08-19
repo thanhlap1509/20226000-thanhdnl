edaa9aa2cd6b760af2dd7086f0487683d3a7a23a
Dinh Ngoc Lap Thanh <thanhdinhngoclap123@gmail.com>
2025-08-19 13:44:19 +0700

    localize usage of catchAsync

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,89 +2,89 @@ const { StatusCodes } = require("http-status-codes");
 const userService = require("../services");
 const { catchAsync } = require("../utils");
 
-const createUser = catchAsync(async (req, res) => {
+const createUser = async (req, res) => {
   const user = await userService.createUser(req.body);
   res.status(StatusCodes.CREATED).send(user);
-});
+};
 
-const getAllUsers = catchAsync(async (req, res) => {
+const getAllUsers = async (req, res) => {
   const { sort_by, limit, offset, ...filter } = req.query;
   const users = await userService.getAllUsers(sort_by, limit, offset, filter);
   res.send(users);
-});
+};
 
-const getUser = catchAsync(async (req, res, next) => {
+const getUser = async (req, res, next) => {
   const user = await userService.getUser(req.params.userId);
   if (user) {
     res.send(user);
   } else {
     next();
   }
-});
+};
 
-const updateUser = catchAsync(async (req, res, next) => {
+const updateUser = async (req, res, next) => {
   const user = await userService.updateUser(req.params.userId, req.body);
   if (user) {
     res.send(user);
   } else {
     next();
   }
-});
+};
 
-const deleteUser = catchAsync(async (req, res, next) => {
+const deleteUser = async (req, res, next) => {
   const user = await userService.deleteUser(req.params.userId);
   if (user) {
     res.status(StatusCodes.NO_CONTENT).send();
   } else {
     next();
   }
-});
+};
 
-const getUserAge = catchAsync(async (req, res, next) => {
+const getUserAge = async (req, res, next) => {
   const user = await userService.getUserAge(req.params.userId);
   if (user) {
     res.send(user);
   } else {
     next();
   }
-});
+};
 
-const getUserCount = catchAsync(async (req, res) => {
+const getUserCount = async (req, res) => {
   const userCount = await userService.getUserCount();
   res.send({ userCount });
-});
+};
 
-const getUserCountByRoles = catchAsync(async (req, res) => {
+const getUserCountByRoles = async (req, res) => {
   const userStats = await userService.getUserCountByRoles();
   res.send(userStats);
-});
+};
 
-const getUserCountByRole = catchAsync(async (req, res) => {
+const getUserCountByRole = async (req, res) => {
   const userStats = await userService.getUserCountByRole(req.params.role);
   return res.send(userStats);
-});
+};
 
-const getUserCountByEmailDomains = catchAsync(async (req, res) => {
+const getUserCountByEmailDomains = async (req, res) => {
   const userStats = await userService.getUserCountByEmailDomains();
   res.send(userStats);
-});
+};
 
-const getUserCountByEmailDomain = catchAsync(async (req, res) => {
+const getUserCountByEmailDomain = async (req, res) => {
   const userStats = await userService.getUserCountByEmailDomain(
     req.params.domain,
   );
   res.send(userStats);
-});
+};
 
-const getTopNEmailDomains = catchAsync(async (req, res) => {
+const getTopNEmailDomains = async (req, res) => {
   res.send(await userService.getUserCountByEmailDomains("des", req.params.n));
-});
+};
 
-const getLastNEmailDomains = catchAsync(async (req, res) => {
+const getLastNEmailDomains = async (req, res) => {
   res.send(await userService.getUserCountByEmailDomains("asc", req.params.n));
-});
+};
 
-module.exports = {
+const exportFuncs = {
   createUser,
   getUser,
   getAllUsers,
@@ -99,3 +99,9 @@ module.exports = {
   getTopNEmailDomains,
   getUserAge,
 };
+
+for (const [fnName, fn] of Object.entries(exportFuncs)) {
+  exportFuncs[fnName] = catchAsync(fn);
+}
+
+module.exports = exportFuncs;
